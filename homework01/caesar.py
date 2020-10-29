40b06e872a40304ffd7a40701b3885b6b95e7eea
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2020-10-29 12:26:09 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -23,14 +23,14 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
             if key > ord("Z"):
                 key = ord("A") + key % 91
                 ciphertext += chr(key)
-                else:
+            else:
                 ciphertext += chr(key)
         elif ord("a") <= ord(i) <= ord("z"):
             key = ord(i) + (shift % 91)                
             if k > ord("z"):
                 key = ord("a") + key % 123
                 ciphertext += chr(key)
-                else:
+            else:
                 ciphertext += chr(key)
         else ciphertext += i
     return ciphertext
@@ -58,14 +58,14 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
             if key > ord("A"):
                 key = ord("Z") + (key - 64)
                 plaintext += chr(key)
-                else:
+            else:
                 plaintext += chr(key)
         elif ord("a") <= ord(i) <= ord("z"):
             key = ord(i) - (shift % 26)
             if key > ord("a"):
                 key = ord("z") + (key - 96)
                 plaintext += chr(key)
-                else:
+            else:
                 plaintext += chr(key)
         else plaintext += i
     return plaintext
