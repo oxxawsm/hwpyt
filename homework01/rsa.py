75336897a0d285e500dcafab4c7b258455d7084f
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2020-10-29 13:10:36 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -68,7 +68,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     n = p * q
 
     # phi = (p-1)(q-1)
-    phi = (p-1) * (q - 1)
+    phi = (p-1) * (q-1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
