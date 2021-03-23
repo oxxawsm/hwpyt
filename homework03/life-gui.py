6a03be41267d6a464940531f01f98f5cdacdcd88
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2021-03-23 16:27:21 +0300

    Update life-gui.py

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 
+i,port life
 from life import GameOfLife
 from ui import UI
 
@@ -9,15 +10,82 @@ class GUI(UI):
 
     def __init__(self, life: GameOfLife, cell_size: int=10, speed: int=10) -> None:
         super().__init__(life)
+        self.cell_size = cell_size
+        self.speed = speed
+        self.width = self.life.cols * cell_size
+        self.height = self.life.rows * cell_size
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
     def draw_lines(self) -> None:
-        # Copy from previous assignment
-        pass
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (x, 0), (x, self.height)
+            )
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (0, y), (self.width, y)
+            )
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
-        pass
+        for i in range(self.life.rows):
+            for j in range(self.life.cols):
+                if self.life.curr_generation[l][k]:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("green"),
+                        (
+                            j * self.cell_size,
+                            i * self.cell_size,
+                            self.cell_size,
+                            self.cell_size,
+                        ),
+                    )
+                else:
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("white"),
+                        pygame.Rect(
+                            j * self.cell_size,
+                            i * self.cell_size,
+                            self.cell_size,
+                            self.cell_size,
+                        ),
+                    )
 
     def run(self) -> None:
-        # Copy from previous assignment
-        pass
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+        running = True
+        pause = False
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    (cell_y, cell_x) = pygame.mouse.get_pos()
+                    self.change_state((cell_x, cell_y))
+                    self.draw_grid()
+                    self.draw_lines()
+                    pygame.display.flip()
+                    clock.tick(self.speed)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        pause = not pause
+            if not pause:
+                self.life.step()
+
+            self.draw_grid()
+            self.draw_lines()
+
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+       
+    
+ def main():
+     game = GameOfLife(size=(48, 64))
+     application = GUI(game)
+     application.run()
+        
