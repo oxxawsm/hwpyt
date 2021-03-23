a5fde5d5cc03cb85d99042ea165d7991fad2a5cb
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2021-03-23 15:42:18 +0300

    Update life-console.py

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -1,4 +1,6 @@
 import curses
+import time
+import pathlib
 
 from life import GameOfLife
 from ui import UI
@@ -8,16 +10,37 @@ class Console(UI):
 
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
+        self.save_path = save_path
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border(0)
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for i in range(1, len(self.life.curr_generation) - 1):
+            for j in range(1, len(self.life.curr_generation[i]) - 1):
+                if self.life.curr_generation[i][j]:
+                    condit = '*'
+                else:
+                    condit = ' '
+                screen.addch(i, j, condit)
 
     def run(self) -> None:
-        screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        screen = curses.initscr().derwin(
+            len(self.life.curr_generation), len(self.life.curr_generation[0]), 0, 0
+        )
+        curses.curs_set(0)
+        running = True
+        while running:
+            screen.clear()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            self.life.step()
+
+            if self.life.max_generation_over:
+                running = False
+            screen.refresh()
+            
+        screen.getch()
         curses.endwin()
