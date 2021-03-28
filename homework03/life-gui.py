bb0c896bca4ea665d6cdcaac32868de3349c5b09
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2021-03-28 17:20:06 +0300

    Update life-gui.py

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -8,15 +8,16 @@ from ui import UI
 
 class GUI(UI):
 
-    def __init__(self, life: GameOfLife, cell_size: int=10, speed: int=10) -> None:
+    def __init__(self, life: GameOfLife, cell_size: int = 30, speed: int=10) -> None:
+        
         super().__init__(life)
+        
         self.cell_size = cell_size
-        self.speed = speed
         self.width = self.life.cols * cell_size
         self.height = self.life.rows * cell_size
-        self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen_size = self.life.cols * cell_size, self.life.rows * cell_size
         self.screen = pygame.display.set_mode(self.screen_size)
+        self.speed = speed
         
 
     def draw_lines(self) -> None:
@@ -24,7 +25,6 @@ class GUI(UI):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
-            
         pass
 
     
@@ -42,7 +42,8 @@ class GUI(UI):
                                      y * self.cell_size + 1,
                                      self.cell_size - 1,
                                      self.cell_size - 1
-                                 ))
+                                 )
+                                 )
                 
             pass
 
@@ -50,8 +51,8 @@ class GUI(UI):
     def run(self) -> None:
         pygame.init()
         clock = pygame.time.Clock()
-        pygame.display.set_caption("Game of Life")
-        self.screen.fill(pygame.Color("white"))
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
         self.is_pause = False
         
         self.grid = self.life.create_grid(randomize = True)
@@ -67,8 +68,10 @@ class GUI(UI):
                         cell = grid[event.pos[1] // self.cell_size][event.pos[0] // self.cell_size]
                         cell = 1 if cell == 0 else 0
                         grid[event.pos[1] // self.cell_size][event.pos[0] // self.cell_size] = cell
+                        
                 if event.type == QUIT:
                     running == False
+                    
             if self.is_pause:
                 self.draw_lines()
                 self.draw_grid()
@@ -83,11 +86,10 @@ class GUI(UI):
         
     
  def main():
-     game = GameOfLife(size=(48, 64))
+     game = GameOfLife(size=(25, 25))
      application = GUI(game)
      application.run()
         
  
 if __name__ == "__main__":
     main()
-        
