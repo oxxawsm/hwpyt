e962aa724e2fd7c28c6835643b13c48e1242c474
Елена Титенко <63160594+elenatitenko@users.noreply.github.com>
2021-05-28 09:26:35 +0300

    Add files via upload

--- homework06/naive_bayes/db.py
+++ homework06/naive_bayes/db.py
@@ -1,1 +1,23 @@
+import os
 
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine(f"sqlite:///{os.path.dirname(os.path.realpath(__file__))}/../news.db")
+session = sessionmaker(bind=engine)
+
+
+class News(Base):  # type: ignore
+    __tablename__ = "news"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    url = Column(String)
+    comments = Column(Integer)
+    points = Column(Integer)
+    label = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
